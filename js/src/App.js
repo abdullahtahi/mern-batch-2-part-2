7759ec0e983fb8840d1f599f0f97a20d00a1988f
unknown <tahirabdullah801@gmail.com>
2021-06-13 21:13:27 +0500

    use state in objects

--- js/src/App.js
+++ js/src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
-import React from'react'
+import React,{ useState } from'react'
 import Header from"./component/header/Header"
 import Home from '../src/pages/home/Home'
 function App() {
+  const [state, setstate] = useState({
+    title:"mouse",
+    price:550,
+    quantity:10
+  })
+   const update=()=>{
+  setstate({...state,price:300}) 
+  }
   return (
     <div className="container">
-       
+{/*        
       <Header/>
-      <Home/>
+      <Home/> */}
+<div>{state.title}</div>     
+<div>{state.price}</div>
+<div>{state.quantity}</div>     
+<button onClick={update}>update price</button>
+
+
     </div>
-  );
+);
 }
 
 export default App;
