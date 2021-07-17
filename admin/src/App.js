414a18675e05476ed143ba4dead631b21826de3d
unknown <tahirabdullah801@gmail.com>
2021-07-17 07:53:13 +0500

    this is  a z-axis work on the product detail page

---
+++ admin/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './componenet/navbar/Navbar'
+import Header from './componenet/header/Header'
+import Home from './pages/home/Home'
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+       <Header />
+      {/*<Home /> */}
+    </div>
+  )
+}
